e8203f53e15a70122011c5a6b822effa96b32c4d
Jon Faustman <faustman@k-state.edu>
2013-06-20 13:00:09 -0500

    Update footer.

--- wp-content/themes/faustman/footer.php
+++ wp-content/themes/faustman/footer.php
@@ -5,43 +5,11 @@
  */
 ?>
 
-<div id="footer">
+    <footer id="footer">
+        <p>Copyright &copy; 2009+ Jon Faustman.</p>
+    </footer>
+</div><!-- /wrapper -->
 
-<?php
-if (is_page('361') OR is_page('422'))
-{
-echo '<p>Copyright &copy; 2009+ Jon Faustman. Views expressed are purely my own.</p>'."\n";
-}
-else
-{
-echo '<p>Copyright &copy; 2009+ Jon Faustman. Views expressed are purely my own, and maybe that crazy dude on the corner. Reppin&rsquo; <a href="http://wordpress.org/">WordPress</a>.</p>'."\n";
-}
-?>
-
-</div>
-
-</div>
-<?php
-if (is_page('361') OR is_page('422'))
-{
-echo '<script type="text/javascript" src="http://dnn506yrbagrg.cloudfront.net/pages/scripts/0009/0017.js"></script>'."\n";
-}
-else
-{
-echo  '<!--<script type="text/javascript" src="http://readernaut.com/api/v1/json/faustman/books/reading/?callback=parseResponse"></script>-->'."\n";
-}
-?>
-<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-5638086-1']);
-  _gaq.push(['_trackPageview']);
-  _gaq.push(['_trackPageLoadTime']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
+<script>var _gaq=[["_setAccount","UA-5638086-1"],["_trackPageview"]];(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.src="//www.google-analytics.com/ga.js";s.parentNode.insertBefore(g,s)}(document,"script"));</script>
 </body>
 </html>
