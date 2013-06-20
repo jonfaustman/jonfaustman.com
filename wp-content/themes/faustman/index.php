5d3f74e0d1dba2d106992e7f5f9b006100b79fe1
Jon Faustman <faustman@k-state.edu>
2013-06-20 12:30:47 -0500

    BEM; HTML5.

--- wp-content/themes/faustman/index.php
+++ wp-content/themes/faustman/index.php
@@ -6,40 +6,32 @@
 
 get_header(); ?>
 
-<div id="content">
+<section id="content">
+    <?php if (have_posts()) : ?>
 
-<?php if (have_posts()) : ?>
-<?php while (have_posts()) : the_post(); ?>
-<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+        <?php while (have_posts()) : the_post(); ?>
+        <article <?php post_class('article') ?> id="post-<?php the_ID(); ?>">
 
-<div class="date"><?php the_time('M') ?><strong><?php the_time('j') ?></strong></div>
+            <time class="article__date date" pubdate datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time('M') ?><i class="article__date--day"><?php the_time('j') ?></i></time>
 
-<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+            <h2 class="article__title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
-<div class="entry">
+            <div class="article__content entry">
+                <?php the_content("&hellip;continue " . get_the_title('', '', false)); echo "\n"; ?>
+            </div><!-- /article__content -->
 
-<?php the_content("&hellip;continue " . get_the_title('', '', false)); echo "\n"; ?>
+        </article>
+        <?php endwhile; ?>
 
-</div>
+    <div class="navigation">
+        <?php posts_nav_link(' &ndash; ', '&laquo; Newer ramblings', 'Older ramblings &raquo;'); echo "\n"; ?>
+    </div>
 
-</div>
+    <?php else : ?>
+        <h2>Not Found</h2>
 
-<?php endwhile; ?>
+        <p>Sorry, but you are looking for something that isn't here.</p>
+    <?php endif; ?>
+</section>
 
-<div class="navigation">
-
-<?php posts_nav_link(' &ndash; ', '&laquo; Newer ramblings', 'Older ramblings &raquo;'); echo "\n"; ?>
-
-</div>
-
-<?php else : ?>
-
-<h2>Not Found</h2>
-<p>Sorry, but you are looking for something that isn't here.</p>
-
-<?php endif; ?>
-
-</div>
-
-<?php //get_sidebar(); ?>
 <?php get_footer(); ?>
