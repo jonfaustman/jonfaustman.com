56ef8b49c48136820a08858ba67ce7071bdac2f7
Jon Faustman <faustman@k-state.edu>
2013-06-20 13:29:44 -0500

    BEM; HTML5.

--- wp-content/themes/faustman/index.php
+++ wp-content/themes/faustman/index.php
@@ -7,31 +7,30 @@
 get_header(); ?>
 
 <section id="content">
-    <?php if (have_posts()) : ?>
+<?php if (have_posts()) : ?>
 
-        <?php while (have_posts()) : the_post(); ?>
-        <article <?php post_class('article') ?> id="post-<?php the_ID(); ?>">
+    <?php while (have_posts()) : the_post(); ?>
+    <article <?php post_class('article') ?> id="post-<?php the_ID(); ?>">
 
-            <time class="article__date date" pubdate datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time('M') ?><i class="article__date--day"><?php the_time('j') ?></i></time>
+        <time class="article__date date" pubdate datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time('M') ?><i class="article__date--day"><?php the_time('j') ?></i></time>
 
-            <h2 class="article__title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+        <h1 class="article__title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 
-            <div class="article__content entry">
-                <?php the_content("&hellip;continue " . get_the_title('', '', false)); echo "\n"; ?>
-            </div><!-- /article__content -->
+        <div class="article__content entry">
+            <?php the_content("&hellip;continue " . get_the_title('', '', false)); echo "\n"; ?>
+        </div><!-- /article__content -->
+    </article>
+    <?php endwhile; ?>
 
-        </article>
-        <?php endwhile; ?>
+<div class="navigation">
+    <?php posts_nav_link(' &ndash; ', '&laquo; Newer ramblings', 'Older ramblings &raquo;'); echo "\n"; ?>
+</div>
 
-    <div class="navigation">
-        <?php posts_nav_link(' &ndash; ', '&laquo; Newer ramblings', 'Older ramblings &raquo;'); echo "\n"; ?>
-    </div>
+<?php else : ?>
+    <h2>Not Found</h2>
 
-    <?php else : ?>
-        <h2>Not Found</h2>
-
-        <p>Sorry, but you are looking for something that isn't here.</p>
-    <?php endif; ?>
+    <p>Sorry, but you are looking for something that isn't here.</p>
+<?php endif; ?>
 </section>
 
 <?php get_footer(); ?>
--- wp-content/themes/faustman/single.php
+++ wp-content/themes/faustman/single.php
@@ -6,62 +6,57 @@
 
 get_header(); ?>
 
-<div id="content">
+<section id="content">
 
-<?php /** Comment out Google Adsense for now
-/*  <script type="text/javascript"><!--
-/*  google_ad_client = "ca-pub-4502909301189493";
-/*  /* Default */
-/*  google_ad_slot = "7581999967";
-/*  google_ad_width = 728;
-/*  google_ad_height = 90;
-/*  //-->
-/*  </script>
-/*  <script type="text/javascript"
-/*  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-/*  </script>
-**/ ?>
+    <?php /** Comment out Google Adsense for now
+    /*  <script type="text/javascript"><!--
+    /*  google_ad_client = "ca-pub-4502909301189493";
+    /*  /* Default */
+    /*  google_ad_slot = "7581999967";
+    /*  google_ad_width = 728;
+    /*  google_ad_height = 90;
+    /*  //-->
+    /*  </script>
+    /*  <script type="text/javascript"
+    /*  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+    /*  </script>
+    **/ ?>
 
 <?php if (have_posts()) : ?>
-<?php while (have_posts()) : the_post(); ?>
-<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+    
+    <?php while (have_posts()) : the_post(); ?>
+    <article <?php post_class('article') ?> id="post-<?php the_ID(); ?>">
 
-<div class="date"><?php the_time('M') ?><strong><?php the_time('j') ?></strong></div>
+        <time class="article__date date" pubdate datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time('M') ?><i class="article__date--day"><?php the_time('j') ?></i></time>
 
-<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+        <h1 class="article__title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 
-<div class="entry">
-<?php the_content('Read the rest of this entry &raquo;'); ?>
-</div>
+        <div class="article__content entry">
+            <?php the_content('Read the rest of this entry &raquo;'); ?>
+        </div><!-- /article__content -->
+    </article>
+    <?php endwhile; ?>
 
-</div>
+    <?php else : ?>
+        <h1>Not Found</h1>
 
-<?php endwhile; ?>
-
-
-<?php else : ?>
-
-<h1>Not Found</h1>
-
-<p>Sorry, but you are looking for something that isn't here.</p>
-
+        <p>Sorry, but you are looking for something that isn't here.</p>
 <?php endif; ?>
 
-<?php /** Comment out Google Adsense for now
-/*  <script type="text/javascript"><!--
-/*  google_ad_client = "ca-pub-4502909301189493";
-/*  /* Default */
-/*  google_ad_slot = "7581999967";
-/*  google_ad_width = 728;
-/*  google_ad_height = 90;
-/*  //-->
-/*  </script>
-/*  <script type="text/javascript"
-/*  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-/*  </script>
-**/ ?>
+    <?php /** Comment out Google Adsense for now
+    /*  <script type="text/javascript"><!--
+    /*  google_ad_client = "ca-pub-4502909301189493";
+    /*  /* Default */
+    /*  google_ad_slot = "7581999967";
+    /*  google_ad_width = 728;
+    /*  google_ad_height = 90;
+    /*  //-->
+    /*  </script>
+    /*  <script type="text/javascript"
+    /*  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+    /*  </script>
+    **/ ?>
 
-</div>
+</section>
 
-<?php //get_sidebar(); ?>
 <?php get_footer(); ?>
