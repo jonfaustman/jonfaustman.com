8535d3c44b03d6505cce51a0ee9c1c5282843fd3
Jon Faustman <faustman@k-state.edu>
2013-08-21 23:03:24 -0500

    Remove ad; adjust style.

--- wp-content/themes/faustman/single.php
+++ wp-content/themes/faustman/single.php
@@ -16,30 +16,6 @@ get_header(); ?>
         
         <time class="article__date" pubdate datetime="<?php the_time('Y-m-d H:i'); ?>"><?php the_time('M') ?><i class="article__date--day"><?php the_time('j') ?></i><i class="article__date--year"><?php the_time('Y') ?></i></time>
         
-        <div id="google-ads-1">
-         <script type="text/javascript">
- 
-        	adUnit   = document.getElementById("google-ads-1");
-        	adWidth  = adUnit.offsetWidth;
- 
-        	google_ad_client = "ca-pub-5996049043771362";
- 
-        	if ( adWidth >= 728 ) {
-        	  google_ad_slot	= "5806617931";
-        	  google_ad_width	= 728;
-        	  google_ad_height 	= 90;
-        	} else {
-        	  google_ad_slot 	= "8963403930";
-        	  google_ad_width 	= 320;
-        	  google_ad_height 	= 50;
-        	} 
-         </script>
- 
-         <script type="text/javascript"    
-           src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-         </script>
-        </div><!--/google-ads-1 -->
-        
         <div class="article__content">
             <?php the_content(); ?>
         </div><!-- /article__content -->
@@ -53,29 +29,22 @@ get_header(); ?>
 <?php endif; ?>
 
     <div id="google-ads-2">
-     <script type="text/javascript">
-
-    	adUnit   = document.getElementById("google-ads-2");
-    	adWidth  = adUnit.offsetWidth;
-
-    	google_ad_client = "ca-pub-5996049043771362";
-
-    	if ( adWidth >= 728 ) {
-    	  google_ad_slot	= "5806617931";
-    	  google_ad_width	= 728;
-    	  google_ad_height 	= 90;
-    	} else {
-    	  google_ad_slot 	= "8963403930";
-    	  google_ad_width 	= 320;
-    	  google_ad_height 	= 50;
-    	} 
-     </script>
-
-     <script type="text/javascript"    
-       src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-     </script>
+        <script type="text/javascript">
+            adUnit   = document.getElementById("google-ads-2");
+            adWidth  = adUnit.offsetWidth;
+            google_ad_client = "ca-pub-5996049043771362";
+            if ( adWidth >= 728 ) {
+                google_ad_slot	= "5806617931";
+                google_ad_width	= 728;
+                google_ad_height 	= 90;
+            } else {
+                google_ad_slot 	= "8963403930";
+                google_ad_width 	= 320;
+                google_ad_height 	= 50;
+            } 
+        </script>
+        <script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
     </div><!--/google-ads-2 -->
-
 </section>
 
 <?php get_footer(); ?>
